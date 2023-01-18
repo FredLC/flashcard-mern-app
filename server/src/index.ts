265fc3f41935c14e2aa0efaf4b819bebd21666dc
Fred Lefebvre <fredlefevre@outlook.com>
2023-01-18 18:05:59 -0500

    Implemented GET /decks endpoint and UI to display cards

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,11 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
+});
+
 app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new Deck({
     title: req.body.title,
