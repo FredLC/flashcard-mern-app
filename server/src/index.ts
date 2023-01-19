1dcdd5d2c7cd51e147103154a0a2d7ced918d04c
Fred Lefebvre <fredlefevre@outlook.com>
2023-01-19 16:43:45 -0500

    Implemented card API endpoints and client functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,13 @@ import { config } from "dotenv";
 config();
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import Deck from "./models/Deck";
 import cors from "cors";
 import { getDecksController } from "./controllers/getDecksController";
+import { getDeckController } from "./controllers/getDeckController";
 import { createDeckController } from "./controllers/createDeckController";
 import { deleteDeckController } from "./controllers/deleteDeckController";
 import { createDeckCardController } from "./controllers/createDeckCardController";
+import { deleteDeckCardController } from "./controllers/deleteDeckCardController";
 
 const app = express();
 const PORT = 5000;
@@ -16,12 +17,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/decks", getDecksController);
-
 app.post("/decks", createDeckController);
-
 app.delete("/decks/:deckId", deleteDeckController);
 
+app.get("/decks/:deckId", getDeckController);
 app.post("/decks/:deckId/cards", createDeckCardController);
+app.delete("/decks/:deckId/cards/:index", deleteDeckCardController);
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`listening on port ${PORT}`);
