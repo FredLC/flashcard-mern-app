78625ba2148b6839e19012825776b8864ece00a2
Fred Lefebvre <fredlefevre@outlook.com>
2023-01-19 11:58:31 -0500

    Added delete /decks endpoint and delete button on deck cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault();
-    await fetch("http://localhost:5000/decks", {
+    const response = await fetch("http://localhost:5000/decks", {
       method: "POST",
       body: JSON.stringify({
         title,
@@ -21,7 +21,16 @@ function App() {
         "Content-Type": "application/json",
       },
     });
+    const deck = await response.json();
+    setDecks([...decks, deck]);
     setTitle("");
+  }
+
+  async function handleDeleteDeck(deckId: string) {
+    await fetch(`http://localhost:5000/decks/${deckId}`, {
+      method: "DELETE",
+    });
+    setDecks(decks.filter((deck) => deck._id !== deckId));
   }
 
   useEffect(() => {
@@ -37,7 +46,10 @@ function App() {
     <div className="App">
       <ul className="decks">
         {decks.map((deck) => (
-          <li key={deck._id}>{deck.title}</li>
+          <li key={deck._id}>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+            {deck.title}
+          </li>
         ))}
       </ul>
       <form onSubmit={handleCreateDeck}>
